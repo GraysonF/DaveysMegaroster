9339c453225a0357af2457bd4f6ce5b00a5f2400
Grayson faircloth <graysonfaircloth@Graysons-MacBook-Pro.local>
2017-05-17 11:04:34 -0400

    Create student object with an id

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 $(document).foundation()
+
+const megaroster = {
+    students: [],
+
+
+
+    init(listSelector) {
+        this.studentList = document.querySelector(listSelector);
+        this.max = 1;
+        document
+        .querySelector('#new-student')
+        .addEventListener('submit', this.addStudent.bind(this));
+    },
+
+    addStudent(ev) {
+        ev.preventDefault();
+        const f = ev.target;
+       // const studentName = f.studentName.value; // or this.studentName.value (not as safe)
+       const student = {
+           name: f.studentName.value,
+           id: this.max++,
+       };
+        this.buildListItem(student);
+    },
+    
+    buildListItem(student) {
+        console.log(student);
+    }
+
+}
+megaroster.init('#studentList');
